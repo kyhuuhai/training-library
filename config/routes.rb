d2c0a3d201fe8faad25fe9d6735375cb5716d1a6
blacktho <vanquytho12345@gmail.com>
2022-04-14 17:20:40 +0700

    manage publisher

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root 'users#index'
   resources :books
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
+  resources :authors
+  namespace :admin do
+    resources :publishers
+    resources :authors
+    resources :categories
+    resources :books
+  end
+  resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
