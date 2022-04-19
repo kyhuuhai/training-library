dd2448730aaf86a4a4bff34946a39e24eded4773
Tran Minh Tien <88039768+minhhtien11@users.noreply.github.com>
2022-04-19 15:38:19 +0700

    search (#18)
    Co-authored-by: rikai-hai <69025485+rikai-hai@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   delete 'logout' => 'sessions#destroy'
   get 'index_category' => 'categories#index'
   resources :users
+  namespace :admin do
+    resources :categories
   resources :authors
   resources :books
   resources :categories
