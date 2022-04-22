7835a8e10fa9804a87194315939c52ab2893d2f2
hoangthai <hoangthai150199@gmail.com>
2022-04-22 10:47:47 +0700

    Add task account activation

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :books
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
   resources :authors
   namespace :admin do
@@ -9,4 +12,5 @@ Rails.application.routes.draw do
     resources :authors
     resources :categories
   end
+  resources :account_activations, only: [:edit]
 end
