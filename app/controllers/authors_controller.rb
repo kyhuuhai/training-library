60884f9c774805e4f9aca4bce441ecb036d65f69
hoangthai <hoangthai150199@gmail.com>
2022-05-05 10:29:15 +0700

    Add code detail author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,4 +2,8 @@ class AuthorsController < ApplicationController
   def index
     @authors = Author.paginate(page: params[:page], per_page: Constant::PER_PAGE)
   end
+  
+  def show
+    @author = Author.find_by_id(params[:id])
+  end
 end
